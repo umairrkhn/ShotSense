cb73b26fdb6a5821d508749e25b945cfee231c90
Umair Ali Khan <umairrkhn@icloud.com>
2023-12-08 01:37:57 +0500

    updated sessions

--- shotsense/lib/screens/sessions.dart
+++ shotsense/lib/screens/sessions.dart
@@ -16,190 +16,251 @@ class _SessionPageState extends State<SessionPage> {
       appBar: AppBar(
         title: const Text('Sessions'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            const ElevatedButton(
-              onPressed: null,
-              child: SizedBox(
-                width: double.infinity,
-                height: 100,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.add_box_rounded, size: 30),
-                    Text(
-                        'Create New Session',
-                        style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black
-                        ),
-                        textAlign: TextAlign.center
-                    ),
-                  ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              const ElevatedButton(
+                onPressed: null,
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 100,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.add_box_rounded, size: 30),
+                      Text(
+                          'Create New Session',
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black
+                          ),
+                          textAlign: TextAlign.center
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 15.0),
-            const Padding(padding: EdgeInsets.all(5.0),
-              child: Text('Current Sessions', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold), textAlign: TextAlign.left),
-            ),
-            Column(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.white),
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  child: Column(
-                    children: [
-                      const SizedBox(height: 15.0),
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.white),
-                          borderRadius: BorderRadius.circular(8.0),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withOpacity(0.2),
-                              spreadRadius: 2,
-                              blurRadius: 10,
-                              offset: const Offset(0, 2),
-                            ),
-                          ],
-                          color: Colors.white,
-                        ),
-                        child: ListTile(
-                          onTap: () {
-                            Navigator.pushNamed(context, '/current_session');
-                          },
-                          leading: ClipRRect(
+              const SizedBox(height: 15.0),
+              const Padding(padding: EdgeInsets.all(5.0),
+                child: Text('Current Sessions', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold), textAlign: TextAlign.left),
+              ),
+              Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 15.0),
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.white),
                             borderRadius: BorderRadius.circular(8.0),
-                            child: Image.asset(
-                              'assets/images/ShotSense-logo.png',
-                              width: 80,
-                              height: 45,
-                              fit: BoxFit.cover,
-                            ),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.2),
+                                spreadRadius: 2,
+                                blurRadius: 10,
+                                offset: const Offset(0, 2),
+                              ),
+                            ],
+                            color: Colors.white,
                           ),
-                          title: const Text('Session in progress'),
-                          subtitle: const Text('8th Dec 2023'),
+                          child: ListTile(
+                            onTap: () {
+                              // Navigator.pushNamed(context, '/current_session');
+                              print('Session tapped');
+                            },
+                            leading: ClipRRect(
+                              borderRadius: BorderRadius.circular(8.0),
+                              child: Image.asset(
+                                'assets/images/ShotSense-logo.png',
+                                width: 80,
+                                height: 45,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            title: const Text('Session in progress'),
+                            subtitle: const Text('8th Dec 2023'),
+                          ),
                         ),
-                      ),
-                      const SizedBox(height: 15.0),
-                    ],
+                        const SizedBox(height: 15.0),
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            ),
-            const Padding(
-              padding: EdgeInsets.all(5.0),
-              child: Text('Previous Sessions', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold), textAlign: TextAlign.left),
-            ),
-            Column(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.white),
-                    borderRadius: BorderRadius.circular(8.0),
+                ],
+              ),
+              const Padding(
+                padding: EdgeInsets.all(5.0),
+                child: Text('Previous Sessions', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold), textAlign: TextAlign.left),
+              ),
+              Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: Column(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.white),
+                            borderRadius: BorderRadius.circular(8.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.2),
+                                spreadRadius: 2,
+                                blurRadius: 10,
+                                offset: const Offset(0, 2),
+                              ),
+                            ],
+                            color: Colors.white,
+                          ),
+                          child: ListTile(
+                            leading: ClipRRect(
+                              borderRadius: BorderRadius.circular(8.0),
+                              child: Image.asset(
+                                'assets/images/ShotSense-logo.png',
+                                width: 80,
+                                height: 45,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            title: const Text('Session 1'),
+                            subtitle: const Text('2nd Nov 2023'),
+                          ),
+                        ),
+                        const SizedBox(height: 15.0),
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.white),
+                            borderRadius: BorderRadius.circular(8.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.2),
+                                spreadRadius: 2,
+                                blurRadius: 10,
+                                offset: const Offset(0, 2),
+                              ),
+                            ],
+                            color: Colors.white,
+                          ),
+                          child: ListTile(
+                            leading: ClipRRect(
+                              borderRadius: BorderRadius.circular(8.0),
+                              child: Image.asset(
+                                'assets/images/ShotSense-logo.png',
+                                width: 80,
+                                height: 45,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            title: const Text('Session 2'),
+                            subtitle: const Text('10th Nov 2023'),
+                          ),
+                        ),
+                        const SizedBox(height: 15.0),
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.white),
+                            borderRadius: BorderRadius.circular(8.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.2),
+                                spreadRadius: 2,
+                                blurRadius: 10,
+                                offset: const Offset(0, 2),
+                              ),
+                            ],
+                            color: Colors.white,
+                          ),
+                          child: ListTile(
+                            leading: ClipRRect(
+                              borderRadius: BorderRadius.circular(8.0),
+                              child: Image.asset(
+                                'assets/images/ShotSense-logo.png',
+                                width: 80,
+                                height: 45,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            title: const Text('Session 3'),
+                            subtitle: const Text('4th Dec 2023'),
+                          ),
+                        ),
+                        const SizedBox(height: 15.0),
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.white),
+                            borderRadius: BorderRadius.circular(8.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.2),
+                                spreadRadius: 2,
+                                blurRadius: 10,
+                                offset: const Offset(0, 2),
+                              ),
+                            ],
+                            color: Colors.white,
+                          ),
+                          child: ListTile(
+                            leading: ClipRRect(
+                              borderRadius: BorderRadius.circular(8.0),
+                              child: Image.asset(
+                                'assets/images/ShotSense-logo.png',
+                                width: 80,
+                                height: 45,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            title: const Text('Session 4'),
+                            subtitle: const Text('8th Dec 2023'),
+                          ),
+                        ),
+                        const SizedBox(height: 15.0),
+                        Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.white),
+                            borderRadius: BorderRadius.circular(8.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.2),
+                                spreadRadius: 2,
+                                blurRadius: 10,
+                                offset: const Offset(0, 2),
+                              ),
+                            ],
+                            color: Colors.white,
+                          ),
+                          child: ListTile(
+                            leading: ClipRRect(
+                              borderRadius: BorderRadius.circular(8.0),
+                              child: Image.asset(
+                                'assets/images/ShotSense-logo.png',
+                                width: 80,
+                                height: 45,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            title: const Text('Session 5'),
+                            subtitle: const Text('12th Dec 2023'),
+                          ),
+                        ),
+                        const SizedBox(height: 15.0),
+                      ],
+                    ),
                   ),
-                  child: Column(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.white),
-                          borderRadius: BorderRadius.circular(8.0),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withOpacity(0.2),
-                              spreadRadius: 2,
-                              blurRadius: 10,
-                              offset: const Offset(0, 2),
-                            ),
-                          ],
-                          color: Colors.white,
-                        ),
-                        child: ListTile(
-                          leading: ClipRRect(
-                            borderRadius: BorderRadius.circular(8.0),
-                            child: Image.asset(
-                              'assets/images/ShotSense-logo.png',
-                              width: 80,
-                              height: 45,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          title: const Text('Session 1'),
-                          subtitle: const Text('2nd Nov 2023'),
-                        ),
-                      ),
-                      const SizedBox(height: 15.0),
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.white),
-                          borderRadius: BorderRadius.circular(8.0),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withOpacity(0.2),
-                              spreadRadius: 2,
-                              blurRadius: 10,
-                              offset: const Offset(0, 2),
-                            ),
-                          ],
-                          color: Colors.white,
-                        ),
-                        child: ListTile(
-                          leading: ClipRRect(
-                            borderRadius: BorderRadius.circular(8.0),
-                            child: Image.asset(
-                              'assets/images/ShotSense-logo.png',
-                              width: 80,
-                              height: 45,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          title: const Text('Session 2'),
-                          subtitle: const Text('10th Nov 2023'),
-                        ),
-                      ),
-                      const SizedBox(height: 15.0),
-                      Container(
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.white),
-                          borderRadius: BorderRadius.circular(8.0),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withOpacity(0.2),
-                              spreadRadius: 2,
-                              blurRadius: 10,
-                              offset: const Offset(0, 2),
-                            ),
-                          ],
-                          color: Colors.white,
-                        ),
-                        child: ListTile(
-                          leading: ClipRRect(
-                            borderRadius: BorderRadius.circular(8.0),
-                            child: Image.asset(
-                              'assets/images/ShotSense-logo.png',
-                              width: 80,
-                              height: 45,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          title: const Text('Session 3'),
-                          subtitle: const Text('4th Dec 2023'),
-                        ),
-                      ),
-                      const SizedBox(height: 15.0),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
