65e7bad9f129d206cfd47aad9af78cb9c7b6f324
Umair Ali Khan <umairrkhn@icloud.com>
2023-12-13 21:07:15 +0500

    added ml dependencies

--- shotsense/macos/Flutter/GeneratedPluginRegistrant.swift
+++ shotsense/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import firebase_auth
 import firebase_core
+import firebase_ml_model_downloader
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FirebaseModelDownloaderPlugin.register(with: registry.registrar(forPlugin: "FirebaseModelDownloaderPlugin"))
 }
