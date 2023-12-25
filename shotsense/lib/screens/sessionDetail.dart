a545e4d934a10cbc669dfe2d71a862d73423aff6
markajino <muhammadfahad870@gmail.com>
2023-12-25 21:51:00 +0500

    feat: add over confirmation

--- shotsense/lib/screens/sessionDetail.dart
+++ shotsense/lib/screens/sessionDetail.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shotsense/screens/addOver.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'package:video_player/video_player.dart';
 
 class SessionDetailScreen extends StatefulWidget {
   const SessionDetailScreen({Key? key}) : super(key: key);
@@ -9,6 +12,145 @@ class SessionDetailScreen extends StatefulWidget {
 }
 
 class _SessionDetailScreenState extends State<SessionDetailScreen> {
+  File? _video;
+  final ImagePicker _imagePicker = ImagePicker();
+  late VideoPlayerController _videoPlayerController;
+  List<VideoPlayerController> _videoPlayerControllers = [];
+
+  // Future<void> _getImageFromCamera() async {
+  //   final pickedFile = await _imagePicker.pickVideo(source: ImageSource.camera);
+
+  //   if (pickedFile != null) {
+  //     setState(() {
+  //       _image = File(pickedFile.path);
+  //     });
+  //   }
+  // }
+
+  Future<void> _getImageFromGallery() async {
+    final pickedFile =
+        await _imagePicker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _video = File(pickedFile.path);
+      });
+    }
+  }
+
+  Future _recordVideo() async {
+    final pickedFile = await _imagePicker.pickVideo(source: ImageSource.camera);
+    if (pickedFile != null) {
+      _video = File(pickedFile.path);
+      _videoPlayerController = VideoPlayerController.file(_video!)
+        ..initialize().then((_) {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('Confirm Video'),
+                content: AspectRatio(
+                  aspectRatio: _videoPlayerController.value.aspectRatio,
+                  child: VideoPlayer(_videoPlayerController),
+                ),
+                actions: <Widget>[
+                  TextButton(
+                    child: Text('Cancel'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  TextButton(
+                    child: Text('Confirm'),
+                    onPressed: () {
+                      setState(() {
+                        _videoPlayerController.pause();
+                        _videoPlayerControllers.add(_videoPlayerController);
+                      });
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        });
+    }
+  }
+
+  Future _pickVideo() async {
+    final pickedFile =
+        await _imagePicker.pickVideo(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      _video = File(pickedFile.path);
+      _videoPlayerController = VideoPlayerController.file(_video!)
+        ..initialize().then((_) {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('Confirm Video'),
+                content: AspectRatio(
+                  aspectRatio: _videoPlayerController.value.aspectRatio,
+                  child: VideoPlayer(_videoPlayerController),
+                ),
+                actions: <Widget>[
+                  TextButton(
+                    child: Text('Cancel'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  TextButton(
+                    child: Text('Confirm'),
+                    onPressed: () {
+                      // Add the controller to the list and start playing
+                      setState(() {
+                        // _videoPlayerController.setLooping(true);
+                        // _videoPlayerController.play();
+
+                        _videoPlayerControllers.add(_videoPlayerController);
+                      });
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        });
+    }
+  }
+
+  void _showImagePickerBottomSheet() {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              leading: Icon(Icons.camera),
+              title: Text('Take a video'),
+              onTap: () {
+                Navigator.pop(context);
+                _recordVideo();
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.image),
+              title: Text('Choose from gallery'),
+              onTap: () {
+                Navigator.pop(context);
+                _pickVideo();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -167,13 +309,14 @@ class _SessionDetailScreenState extends State<SessionDetailScreen> {
                                     color: Color.fromARGB(255, 11, 11, 11))),
                             const Icon(Icons.arrow_drop_down_rounded, size: 50),
                             const SizedBox(width: 200),
-                            InkWell(
+                            GestureDetector(
                               onTap: () => {
-                                Navigator.push(context, MaterialPageRoute(
-                                  builder: (context) {
-                                    return const AddOver();
-                                  },
-                                ))
+                                // Navigator.push(context, MaterialPageRoute(
+                                //   builder: (context) {
+                                //     return const AddOver();
+                                //   },
+                                // ))
+                                _showImagePickerBottomSheet()
                               },
                               child: const Icon(
                                 Icons.add_circle,
@@ -192,141 +335,80 @@ class _SessionDetailScreenState extends State<SessionDetailScreen> {
               // Balls in the over section
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Row(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.only(bottom: 5),
-                      width: 45,
-                      height: 45,
-                      decoration: BoxDecoration(
-                        image: const DecorationImage(
-                          image: AssetImage('assets/images/BallIcon.png'),
-                          fit: BoxFit.cover,
+                for (var videoPlayerController
+                    in _videoPlayerControllers.asMap().entries)
+                  Row(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(bottom: 5),
+                        width: 45,
+                        height: 45,
+                        decoration: BoxDecoration(
+                          image: const DecorationImage(
+                            image: AssetImage('assets/images/BallIcon.png'),
+                            fit: BoxFit.cover,
+                          ),
+                          borderRadius: BorderRadius.circular(10.0),
                         ),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      child: const Center(
-                        child: Text(
-                          '1.1',
-                          style: TextStyle(
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
+                        child: Center(
+                          child: Text(
+                            _videoPlayerControllers
+                                .asMap()
+                                .entries
+                                .map((entry) => "${entry.key + 1}")
+                                .join(" "),
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    Padding(
-                        padding: EdgeInsets.only(left: 20, right: 10),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                              width: 280,
-                              child: const Text(
-                                "Backward defense against a turning delivery",
-                                softWrap: true,
-                                overflow: TextOverflow.clip,
-                                style: TextStyle(
-                                    fontSize: 15, color: Colors.black),
+                      Padding(
+                          padding: EdgeInsets.only(left: 20, right: 10),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                width: 280,
+                                child: const Text(
+                                  "Backward defense against a turning delivery",
+                                  softWrap: true,
+                                  overflow: TextOverflow.clip,
+                                  style: TextStyle(
+                                      fontSize: 15, color: Colors.black),
+                                ),
                               ),
-                            ),
-                            const Row(
-                              children: [
-                                Text(
-                                  "May 1 2022",
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      color: Color.fromARGB(187, 21, 30, 35)),
-                                ),
-                                SizedBox(width: 5),
-                                Text(
-                                  "•",
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      color: Color.fromARGB(187, 21, 30, 35)),
-                                ),
-                                SizedBox(width: 5),
-                                Text(
-                                  "Defense Shot",
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      color: Color.fromARGB(187, 21, 30, 35)),
-                                ),
-                              ],
-                            )
-                          ],
-                        ))
-                  ],
-                ),
+                              const Row(
+                                children: [
+                                  Text(
+                                    "May 1 2022",
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        color: Color.fromARGB(187, 21, 30, 35)),
+                                  ),
+                                  SizedBox(width: 5),
+                                  Text(
+                                    "•",
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        color: Color.fromARGB(187, 21, 30, 35)),
+                                  ),
+                                  SizedBox(width: 5),
+                                  Text(
+                                    "Defense Shot",
+                                    style: TextStyle(
+                                        fontSize: 15,
+                                        color: Color.fromARGB(187, 21, 30, 35)),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ))
+                    ],
+                  ),
                 SizedBox(height: 15.0),
-                Row(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.only(bottom: 5),
-                      width: 45,
-                      height: 45,
-                      decoration: BoxDecoration(
-                        image: const DecorationImage(
-                          image: AssetImage('assets/images/BallIcon.png'),
-                          fit: BoxFit.cover,
-                        ),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      child: const Center(
-                        child: Text(
-                          '1.2',
-                          style: TextStyle(
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                        padding: EdgeInsets.only(left: 20, right: 10),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                              width: 280,
-                              child: const Text(
-                                "Square cut to a wide delivery",
-                                softWrap: true,
-                                overflow: TextOverflow.clip,
-                                style: TextStyle(
-                                    fontSize: 15, color: Colors.black),
-                              ),
-                            ),
-                            const Row(
-                              children: [
-                                Text(
-                                  "May 1 2022",
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      color: Color.fromARGB(187, 21, 30, 35)),
-                                ),
-                                SizedBox(width: 5),
-                                Text(
-                                  "•",
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      color: Color.fromARGB(187, 21, 30, 35)),
-                                ),
-                                SizedBox(width: 5),
-                                Text(
-                                  "Square Cut Shot",
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      color: Color.fromARGB(187, 21, 30, 35)),
-                                ),
-                              ],
-                            )
-                          ],
-                        ))
-                  ],
-                )
               ],
             ),
           ],
