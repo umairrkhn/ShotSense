f32d6122f933f4e9160a8bbefd065dcb3d67d8b0
markajino <muhammadfahad870@gmail.com>
2024-01-17 19:46:32 +0500

    session detail page fixed

--- shotsense/lib/classes/session.dart
+++ shotsense/lib/classes/session.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'ball.dart';
-class session{
+
+class session {
+  late String id;
   late String name;
   late String userID;
   late Timestamp createdAt;
@@ -8,6 +10,7 @@ class session{
   List<ball> balls = [];
 
   session({
+    required this.id,
     required this.name,
     required this.userID,
     required this.createdAt,
@@ -15,8 +18,9 @@ class session{
     required this.balls,
   });
 
-  Map<String, dynamic> toMap(){
-    return{
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
       'name': name,
       'userID': userID,
       'createdAt': createdAt,
@@ -25,8 +29,9 @@ class session{
     };
   }
 
-  factory session.fromMap(Map<String, dynamic> map){
+  factory session.fromMap(Map<String, dynamic> map) {
     return session(
+      id: map['id'],
       name: map['name'],
       userID: map['userID'],
       createdAt: map['createdAt'],
