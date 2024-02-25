3ae9ed918e408bca7176b788d73a48f1f1a53a36
markajino <muhammadfahad870@gmail.com>
2024-02-25 18:44:33 +0500

    backend folder created

---
+++ Backend/predictions.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask, request, jsonify
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'})
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'})
+
+    if file:
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return jsonify({'message': 'File uploaded successfully', 'filename': filename})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
